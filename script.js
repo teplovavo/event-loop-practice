17c295c9ba3662913ebfdbd462b42dfadd2118e6
Veronika Teplova <teplovavo@gmail.com>
2024-10-04 19:28:44 -0700

    try/catch

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("Start practice!")
 
-
+//////////////////////////////////////////////////////////////////////////
 console.log("Part 1: Stack Overflow")
 
 // Declare a counter to track the number of recursive calls
@@ -11,3 +11,14 @@ const overflowTest = () => {
     count++; // Increment the counter on each call
     overflowTest(); // Call the function 
   };
+
+// Use try/catch to catch the stack overflow error
+try {
+    overflowTest(); // This will keep calling until it hits the maximum stack size
+  } catch (error) {
+    console.log(`Stack overflow after ${count} calls`); // Log the error and how many calls happened
+  }
+
+
+
+  
