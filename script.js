c6a552fbdb5969123407e795020468b8c0215722
Veronika Teplova <teplovavo@gmail.com>
2024-10-04 19:31:09 -0700

    part2

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-console.log("Start practice!")
+console.log("Start practice!");
+
+
+
+
 
 //////////////////////////////////////////////////////////////////////////
-console.log("Part 1: Stack Overflow")
+console.log("Part 1: Stack Overflow");
 
 // Declare a counter to track the number of recursive calls
 let count = 0;
@@ -21,4 +25,6 @@ try {
 
 
 
-  
+
+  //////////////////////////////////////////////////////////////////////////
+console.log("Part 2: Trampolines");
