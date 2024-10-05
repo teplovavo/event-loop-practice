1cc31af11f7e7b1c8e0c2ad92f1301d888e7f305
Veronika Teplova <teplovavo@gmail.com>
2024-10-04 19:27:58 -0700

    step 1 function

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ console.log("Part 1: Stack Overflow")
 
 // Declare a counter to track the number of recursive calls
 let count = 0;
+
+// Recursive function to cause stack overflow
+const overflowTest = () => {
+    count++; // Increment the counter on each call
+    overflowTest(); // Call the function 
+  };
